5dc4c52abb1e5d3830c7956ae519ba5f8f9354e7
muggy8 <muggy8@gmail.com>
2018-06-22 00:41:36 -0700

    really close to things working

--- xhr.js
+++ xhr.js
@@ -1,32 +1,46 @@
-function xhr(){
-	// we use the old version of array from to maintain as much backwards compatibility as we can
-	XMLHttpRequest.apply(this, Array.prototype.slice.call(arguments))
+function xhr (){
+	this._XMLHttpRequest = new XMLHttpRequest()
 }
-xhr.prototype = Object.create(XMLHttpRequest.prototype)
 for(
 	var sourceProto = XMLHttpRequest.prototype;
 	sourceProto;
-	sourceProto = Object.getPrototypeOf(XMLHttpRequest.prototype)
+	sourceProto = Object.getPrototypeOf(sourceProto)
 	// this is the same as a while loop but it's got less characters in it and we're trying to save space so ya
 	// this loop basically iterates over the target prototype and all sub prototypes of that prototype till the end
 ){
 	for(var key in sourceProto){
-		if (typeof sourceProto === "function"){
-			Object.defineProperty(xhr.prototype, key, {
-				enumerable: sourceProto.propertyIsEnumerable(key),
-				value: (function(protoFn){ // we do this so we can keep a reference to the original function once the key property has changed because of the next itteration of this loop
-					return function(){
-						var args = Array.prototype.slice.call(arguments)
-						for(var i = 0; i < args.length; i++){
-							if (typeof args[i] === "function"){
-								args[i] = args[i].bind(this)
-							}
-						}
-						var results = protoFn.apply(this, args)
-						return (typeof results === "undefined") ? this : results
-					}
-				})(sourceProto[key])
-			})
+		if (xhr.prototype.hasOwnProperty(key)){
+			continue
 		}
+		// console.log(sourceProto.hasOwnProperty(key), key, Object.getOwnPropertyNames(sourceProto))
+		Object.defineProperty(xhr.prototype, key, {
+			enumerable: sourceProto.propertyIsEnumerable(key),
+			// configurable: true,
+			get: (function(staticKey){
+				// we do this so we can keep a reference to the original function once the key property has changed because of the next itteration of this loop
+				return function(){
+					var value = this[staticKey]
+					if (typeof staticKey === "function"){
+						return function(){
+							var args = Array.prototype.slice.call(arguments)
+							for(var i = 0; i < args.length; i++){
+								if (typeof args[i] === "function"){
+									args[i] = args[i].bind(this)
+								}
+							}
+							var result = this._XMLHttpRequest[staticKey].apply(this, args)
+							return (typeof result === "undefined") ? this :result
+						}
+					}
+					return value;
+
+				}
+			})(sourceProto, key),
+			set: (function( staticKey){
+				return function(val){
+					this._XMLHttpRequest[staticKey] = val;
+				}
+			})(sourceProto, key)
+		})
 	}
 }
