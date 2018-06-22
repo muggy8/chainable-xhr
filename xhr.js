95c1199892b4778f1af0fe5582f663ba620ec8b7
muggy8 <muggy8@gmail.com>
2018-06-22 01:10:28 -0700

    successfully wrapped the function so it can wrap any other class that has properties that should be editable

--- xhr.js
+++ xhr.js
@@ -1,10 +1,13 @@
-function xhr (){
-	this._XMLHttpRequest = new XMLHttpRequest()
-}
-(function(){
 
+var chainableify = function(classToWrap){
+	var sourceInstanceKey = ("0000" + ((Math.random() * Math.pow(36, 4)) | 0).toString(36)).slice(-4)
+	function wrapper (){
+		Object.defineProperty(this, sourceInstanceKey, {
+			value: new classToWrap()
+		})
+	}
 	for(
-		var sourceProto = XMLHttpRequest.prototype;
+		var sourceProto = classToWrap.prototype;
 		sourceProto;
 		sourceProto = Object.getPrototypeOf(sourceProto)
 		// this is the same as a while loop but it's got less characters in it and we're trying to save space so ya
@@ -13,17 +16,18 @@ function xhr (){
 		var keys = Object.getOwnPropertyNames(sourceProto)
 		for(var i = 0; i < keys.length; i++){
 			var key = keys[i]
-			if (Object.getOwnPropertyNames(xhr.prototype).indexOf(key) > -1){
+			if (Object.getOwnPropertyNames(wrapper.prototype).indexOf(key) > -1){
+				// we check it this way cuz eventually the "hasownproperty method is gonnag get replaced by a getter so it becomes unreliable"
 				continue
 			}
 			// console.log(key)
-			Object.defineProperty(xhr.prototype, key, {
+			Object.defineProperty(wrapper.prototype, key, {
 				// enumerable: sourceProto.propertyIsEnumerable(key),
 				// configurable: true,
 				get: (function(staticKey){
 					// we do this so we can keep a reference to the original function once the key property has changed because of the next itteration of this loop
 					return function(){
-						var value = this._XMLHttpRequest[staticKey]
+						var value = this[sourceInstanceKey][staticKey]
 						if (typeof value === "function"){
 							return function(){
 								var args = Array.prototype.slice.call(arguments)
@@ -32,20 +36,21 @@ function xhr (){
 										args[i] = args[i].bind(this)
 									}
 								}
-								var result = this._XMLHttpRequest[staticKey].apply(this._XMLHttpRequest, args)
+								var result = this[sourceInstanceKey][staticKey].apply(this[sourceInstanceKey], args)
 								return (typeof result === "undefined") ? this :result
 							}
 						}
-						return value;
-
+						return value
 					}
 				})(key),
 				set: (function(staticKey){
 					return function(val){
-						this._XMLHttpRequest[staticKey] = val;
+						this[sourceInstanceKey][staticKey] = val
 					}
 				})(key)
 			})
 		}
 	}
-})()
+
+	return wrapper
+}
